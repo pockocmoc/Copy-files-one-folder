aaeaffd66c03ea7d780412aa9ded67d14583ebeb
pockocmoc <pockocmoc83@gmail.com>
2023-09-20 14:11:03 +0300

    version 0.0.3

--- src/main/java/com/pockocmoc/CopyFiles.java
+++ src/main/java/com/pockocmoc/CopyFiles.java
@@ -2,6 +2,8 @@ package com.pockocmoc;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -40,6 +42,15 @@ public class CopyFiles {
                 try {
                     if (copy.createNewFile()) {
                         resultArea.append("Файл " + f.getName() + " создан.\n");
+                        FileInputStream inputStream = new FileInputStream(f);
+                        FileOutputStream outputStream = new FileOutputStream(copy);
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = inputStream.read(buffer)) > 0) {
+                            outputStream.write(buffer, 0, length);
+                        }
+                        inputStream.close();
+                        outputStream.close();
                     } else {
                         resultArea.append("Ошибка при создании файла " +
                                 f.getName() + "\n");
